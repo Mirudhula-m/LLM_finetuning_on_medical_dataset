5156fcda403c3655622e1559414d27b7d8d4713e
mirudhula <mirudhula.tm@gmail.com>
2024-12-25 17:09:59 -0800

    Send model yaml file to S3 for training param

--- src/setup.py
+++ src/setup.py
@@ -1,4 +1,10 @@
 from sagemaker_setup import setup_sagemaker
+from sagemaker.s3 import S3Uploader
+
+def model_yaml_2_S3():
+    train_config_s3_path = S3Uploader.uppload(local_path=CFG.model_yaml, desired_s3_uri=f"{CFG.s3_save_path}/config")
+    print(f"Training config uploaded to:", train_config_s3_path)
+    return train_config_s3_path
 
 class CFG:
 
@@ -13,4 +19,11 @@ class CFG:
     # AWS SAGEMAKER RELATED CONFIG
 
     sess = setup_sagemaker()
+
+    # AWS S3 RELATED CONFIG
+
     s3_save_path = f"s3://{sess.default_bucket()}/datasets/llama3"
+    model_yaml = "llama_3_70b_fsdp_qlora.yaml"
+    train_config_s3_path = model_yaml_2_S3()
+    
+    
